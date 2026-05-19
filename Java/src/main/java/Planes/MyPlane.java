73fef299a7ff271103eb28937252bfbfc19847eb
kennybeeutomo <kennybee132@gmail.com>
2026-05-19 19:21:07 +0700

    before refactor

--- Java/src/main/java/Planes/MyPlane.java
+++ Java/src/main/java/Planes/MyPlane.java
@@ -1,9 +1,9 @@
 package Planes;
 
-public class MyPlane  extends Plane {
-    public MyPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
-        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-    }
+public class MyPlane extends Plane {
+	public MyPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
+		super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+	}
 
-    //TODO implement it later
+	// TODO implement it later
 }
